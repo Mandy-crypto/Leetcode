128758f885312c00061d838bd7abe52082e33216
Mandy-crypto <gsmani116@gmail.com>
2026-08-07 11:42:01 +0530

    Time: 0 ms (100%), Space: 16.6 MB (86.76%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        queue<pair<int,int>> q;
+        int fresh = 0;
+
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2) q.push({i,j});
+                else if(grid[i][j]==1) fresh++;
+            }
+        if(fresh==0) return 0; 
+        int minutes = 0;
+        int dx[] = {0,0,1,-1};
+        int dy[] = {1,-1,0,0};
+
+        while(!q.empty()){
+            int sz = q.size();
+            bool rottedThisRound = false;
+
+            for(int k=0;k<sz;k++){
+                auto [x,y] = q.front(); q.pop();
+                for(int d=0;d<4;d++){
+                    int nx=x+dx[d], ny=y+dy[d];
+                    if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny]==1){
+                        grid[nx][ny]=2;
+                        fresh--;
+                        q.push({nx,ny});
+                        rottedThisRound = true;
+                    }
+                }
+            }
+            if(rottedThisRound) minutes++;
+        }
+
+        return (fresh==0) ? minutes : -1;
+    }
+};
