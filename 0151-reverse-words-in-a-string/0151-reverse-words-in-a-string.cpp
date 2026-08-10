dff467496c0b3115a8f9a978e91065f526e4a22e
Mandy-crypto <gsmani116@gmail.com>
2026-08-11 01:45:32 +0530

    Time: 0 ms (100%), Space: 9.8 MB (98.54%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        // Step 1: Remove extra spaces in-place (leading, trailing, multiple between words)
-        int n = s.size();
-        int slow = 0; // pointer to write position
-
-        // Skip leading spaces
-        int i = 0;
-        while (i < n) {
-            // skip spaces before a word
-            while (i < n && s[i] == ' ') i++;
-            if (i >= n) break;
-
-            // copy word
-            if (slow != 0) s[slow++] = ' '; // separator before word (except first)
-            while (i < n && s[i] != ' ') {
-                s[slow++] = s[i++];
-            }
+        int n = s.length();
+        int slow = 0;
+        int k = 0;
+        while (k < n) {
+            while (k < n && s[k] == ' ') k++;
+            if (k >= n) break;
+            if (slow != 0) s[slow++] = ' ';
+            while (k < n && s[k] != ' ') s[slow++] = s[k++];
         }
-        s.resize(slow); // trim the unused tail
+        s.resize(slow);
+        n = s.length();
 
-        // Step 2: Reverse the entire cleaned string
         reverse(s.begin(), s.end());
 
-        // Step 3: Reverse each word back to normal
-        int start = 0;
-        for (int j = 0; j <= (int)s.size(); j++) {
-            if (j == (int)s.size() || s[j] == ' ') {
-                reverse(s.begin() + start, s.begin() + j);
-                start = j + 1;
-            }
+        int i = 0, j = 0;
+        while (i < n)
+        {
+            j = i;
+            while (j < n && s[j] != ' ') j++;
+            reverse(s.begin() + i, s.begin() + j);
+            i = j + 1;
         }
 
         return s;
